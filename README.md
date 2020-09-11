2592d93781e972928c02964901deaa09abe9cfd2
Christian Poon <christianxp27@yahoo.com>
2020-09-10 17:05:18 -0700

    create README.md, update welcome background and projects/images

---
+++ README.md
@@ -0,0 +1,22 @@
+# PoonChristian.github.io
+
+Single-Page personal portfolio created only with HTML, CSS, and Vanilla JavaScript (https://poonchristian.github.io/).
+
+# What I Learned
+
+* HTML5 Semantic Elements
+* CSS Flexbox and Grid
+* Google Fonts
+* Hover effects
+* Box Model Styling
+* Centering Content
+* Responsive Design with Media Queries
+* Parallax/Smooth Scrolling
+* Setting active navbar links on scroll
+
+# TO-DO
+
+* Add more projects
+* Add a contact form
+* Use React to organize page into components
+* Adjust custom CSS and use Bootstrap / Semantic UI
--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,51 @@
-(function() {
-    const navbarLinks = document.querySelectorAll("#navbar a");
+(function () {
+  const navbarLinks = document.querySelectorAll("#navbar a");
 
-    // Add scroll event listener for entire window
-    window.addEventListener("scroll", changeActiveNavOnScroll);
+  // Add scroll event listener for entire window
+  window.addEventListener("scroll", changeActiveNavOnScroll);
 
-    // Set active links depending on the scroll position from the top of the screen
-    // Scroll position must be between the top of the section and bottom of the section for section to be active
-    function changeActiveNavOnScroll() {
-        const positionFromTop = this.scrollY;
+  // Set active links depending on the scroll position from the top of the screen
+  // Scroll position must be between the top of the section and bottom of the section for section to be active
+  function changeActiveNavOnScroll() {
+    const positionFromTop = this.scrollY;
 
-        navbarLinks.forEach(link => {
-            let section = document.querySelector(link.hash);
-    
-            if (section.offsetTop - 1 <= positionFromTop && section.offsetTop + section.offsetHeight > positionFromTop) {
-                const active = document.querySelectorAll(".active");
+    navbarLinks.forEach((link) => {
+      let section = document.querySelector(link.hash);
 
-                active.forEach(aLink => {
-                    aLink.classList.remove("active");
-                });
-
-                link.classList.add("active");
-
-                // store the link in local storage to keep track of last visited section
-                localStorage.active = link;
-            } else {
-                link.classList.remove("active");
-            }
-        });
-    }
-    
-    function removeActive() {
+      if (
+        section.offsetTop - 1 <= positionFromTop &&
+        section.offsetTop + section.offsetHeight > positionFromTop
+      ) {
         const active = document.querySelectorAll(".active");
 
-        active.forEach(aLink => {
-            aLink.classList.remove("active");
+        active.forEach((aLink) => {
+          aLink.classList.remove("active");
         });
-    }
 
-    // on page refresh, highlight whatever section the user last visited
-    document.addEventListener("DOMContentLoaded", function() {
-        navbarLinks.forEach(link => {
-            if (link.href === localStorage.active) {
-                link.classList.add("active");
-            }
-        });
-    });  
+        link.classList.add("active");
+
+        // store the link in local storage to keep track of last visited section
+        localStorage.active = link;
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
+
+  function removeActive() {
+    const active = document.querySelectorAll(".active");
+
+    active.forEach((aLink) => {
+      aLink.classList.remove("active");
+    });
+  }
+
+  // on page refresh, highlight whatever section the user last visited
+  document.addEventListener("DOMContentLoaded", function () {
+    navbarLinks.forEach((link) => {
+      if (link.href === localStorage.active) {
+        link.classList.add("active");
+      }
+    });
+  });
 })();
